7173c7695b068e6bc571edf96d10827ca4fd523b
youngbloodcyb <cameron.youngblood@gmail.com>
2022-10-21 18:46:57 -0600

    make new page and add user email feedback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import PocketBase from  '/Users/youngbloodcyb/Projects/pocketbase-test/node_modules/pocketbase';
+    import { goto } from '$app/navigation';
+    import { userAuthData } from "../stores/user";
+
+    const client = new PocketBase('http://127.0.0.1:8090');
+
+    const login = async () => {
+		const userAuth = await client.users.authViaEmail(state.email, state.password);
+		userAuthData.set(userAuth);
+
+		state.email = "";
+		state.password = "";
+
+        console.log($userAuthData);
+        if (userAuth) {
+            goto("/write");
+        }
+	} 
+
+	let state = {
+		email: "",
+		password: ""
+	}
+</script>
+
+<h1 class="text-lg">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form 
+    action="login"
+    method="post"
+    on:submit|preventDefault={login}
+	>
+    <div>
+        <input type="text" id="email" name="email" placeholder="email" required="" bind:value={state.email}>
+        <label for="name">email</label>
+    </div>
+    <div>
+        <input type="password" id="password" name="password" placeholder="password" required="" bind:value={state.password}>
+        <label for="email">password</label>
+    </div>
+    <button type="submit">submit</button>
+</form>
---
+++ src/routes/write/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { userAuthData } from "../../stores/user";
+
+    // const logout = () => {
+    //     client.authStore.clear();
+    // }
+
+</script>
+<h1>Test</h1>
+<h1>You are logged in, {$userAuthData.user.email}</h1>
+<!-- <button on:submit={logout} class="border-solid border-black border-2 p-3 rounded-md" type="submit">Logout</button> -->
