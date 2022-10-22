412e3de74e4033065686a19c789c710e6300821b
youngbloodcyb <cameron.youngblood@gmail.com>
2022-10-21 18:00:42 -0600

    add tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,27 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+		],
 };
 
 export default config;
+
+
+// import preprocess from "svelte-preprocess";
+
+// const config = {
+//   preprocess: [
+//     preprocess({
+//       postcss: true,
+//     }),
+//   ],
+// }
